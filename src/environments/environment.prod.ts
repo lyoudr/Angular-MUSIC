e67f2f8b6fa7336cd74a377d5a93ea49fd9db113
Ann.Ke <ann.ke@cloud-interactive.com>
2021-07-22 10:14:38 +0800

    modify

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
-    apiUrl: 'http://ec2-54-168-171-36.ap-northeast-1.compute.amazonaws.com' // dev ec2 url
+    apiUrl: 'http://ec2-54-95-126-114.ap-northeast-1.compute.amazonaws.com' // dev ec2 url
   };
   
